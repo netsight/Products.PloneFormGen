f9cd64d9bc7809b99ab253f650674a310f6d2cd0
Matthew Sital-Singh <mattss@netsight.co.uk>
2012-12-19 12:54:19 +0000

    Reorder the methods

--- Products/PloneFormGen/interfaces/statefulAdapter.py
+++ Products/PloneFormGen/interfaces/statefulAdapter.py
@@ -4,8 +4,8 @@ from zope.interface import Interface
 class IStatefulActionAdapter(Interface):
     """ an action adapter that stores values for later editing """
 
-    def getExistingValue(field, userkey):
-        """ retrieve any existing value for the given field and user"""
+    def hasExistingValuesFor(userkey):
+        """ are there existing submission values for the given userkey? """
 
-    def hasExistingValues(userkey):
-        """ are there existing submission values for the given user? """
+    def getExistingValueFor(field, userkey):
+        """ retrieve any existing value for the given userkey and field"""
